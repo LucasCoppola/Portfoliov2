a47c9ebcbb4abc01d2b689aefd47187f539cbae7
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-07-18 14:33:37 -0300

    deleted default export

--- components/shared/icons/index.tsx
+++ components/shared/icons/index.tsx
@@ -1,5 +1,4 @@
 export { default as LoadingDots } from "./loading-dots";
 export { default as LoadingCircle } from "./loading-circle";
 export { default as LoadingSpinner } from "./loading-spinner";
-export { default as Arrow } from "./arrow";
 export { default as Github } from "./github";
