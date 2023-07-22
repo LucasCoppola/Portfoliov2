5cafb510360090f8e7a5a7c8783ea1b070a0fabd
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-07-22 12:00:35 -0300

    refactor

--- components/shared/icons/index.tsx
+++ components/shared/icons/index.tsx
@@ -1,5 +1,2 @@
-export { default as LoadingDots } from "./loading-dots";
-export { default as LoadingCircle } from "./loading-circle";
-export { default as LoadingSpinner } from "./loading-spinner";
 export { default as Github } from "./github";
 export { default as ExternalLink } from "./external-link";
